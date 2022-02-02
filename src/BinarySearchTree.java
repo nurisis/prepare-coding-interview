c2e98da892ec90aa98ab6fa0be005e7c96784f20
nurisis <isoyeong666@naver.com>
2022-02-02 21:53:56 +0900

    Implement Binary Search Tree

---
+++ src/BinarySearchTree.java
@@ -0,0 +1,112 @@
+class Node{
+    Node left;
+    int data;
+    Node right;
+    Node(int data){
+        this.data=data;
+    }
+}
+
+public class BinarySearchTree {
+    Node root;
+
+    BinarySearchTree(Node root){
+        this.root = root;
+    }
+
+    public void insert(int key) {
+        Node leafNode = root;
+        Node newNode = new Node(key);
+
+        if (root == null) {
+            root = newNode;
+            return;
+        }
+
+        // Find leaf node
+        while (leafNode != null) {
+            if (leafNode.data == key) {
+                return;
+            } else if (leafNode.data > key) {
+                if (leafNode.left == null) {
+                    leafNode.left = newNode;
+                    return;
+                } else {
+                    leafNode = leafNode.left;
+                }
+            } else {
+                if (leafNode.right == null) {
+                    leafNode.right = newNode;
+                    return;
+                } else {
+                    leafNode = leafNode.right;
+                }
+            }
+        }
+
+    }
+
+    // Function to delete a node from BST.
+    // 노드 삭제 후 root 노드를 반환함.
+    public Node deleteNode(Node root, int X) {
+        if (root == null) return null;
+
+        if (root.data > X) {
+            root.left = deleteNode(root.left, X);
+            return root;
+        } else if (root.data < X) {
+            root.right = deleteNode(root.right, X);
+            return root;
+        }
+
+        if (root.left == null) {
+            return root.right;
+        } else if (root.right == null) {
+            return root.left;
+        }
+
+        // Find smallest node in right child tree
+        Node succParent = root;
+        Node succ = root.right;
+
+        while (succ.left != null) {
+            succParent = succ;
+            succ = succ.left;
+        }
+
+        if (succParent == root) {
+            root.right = succ.right;
+        } else {
+            succParent.left = succ.right;
+        }
+
+        root.data = succ.data;
+
+        return root;
+    }
+
+    public void inorderTraversal(Node root) {
+        if (root == null) return;
+
+        // inorder traversal for left sub tree
+        inorderTraversal(root.left);
+        System.out.print(root.data + " ");
+        inorderTraversal(root.right);
+    }
+
+    public void preOrderTraversal(Node root) {
+        if (root == null) return;
+
+        System.out.print(root.data + " ");
+        preOrderTraversal(root.left);
+        preOrderTraversal(root.right);
+    }
+
+    public void postOrderTraversal(Node root) {
+        if (root == null) return;
+
+        postOrderTraversal(root.left);
+        postOrderTraversal(root.right);
+        System.out.print(root.data + " ");
+    }
+}
---
+++ src/Executor.java
@@ -0,0 +1,83 @@
+public class Executor {
+
+    public static void main(String args[]) {
+        executeBinarySearchTree();
+    }
+
+    private static void executeBinarySearchTree() {
+         /* Let us create following BST
+                  50
+                /     \
+               30     70
+              / \    / \
+             20 40  60 80
+                  \
+                  45
+                 /
+               42
+         */
+        Node root = null;
+        BinarySearchTree binarySearchTree = new BinarySearchTree(root);
+        binarySearchTree.insert(50);
+        binarySearchTree.insert(50);
+        binarySearchTree.insert(30);
+        binarySearchTree.insert(20);
+        binarySearchTree.insert(40);
+        binarySearchTree.insert(70);
+        binarySearchTree.insert(60);
+        binarySearchTree.insert(80);
+        binarySearchTree.insert(45);
+        binarySearchTree.insert(42);
+
+        System.out.println("After inserting");
+        binarySearchTree.inorderTraversal(binarySearchTree.root);
+
+        /* After removing 30
+                  50
+                /     \
+               40     70
+              / \    / \
+             20 45  60 80
+               /
+              42
+         */
+        binarySearchTree.deleteNode(binarySearchTree.root, 30);
+        System.out.println("\nAfter deleting 30");
+        binarySearchTree.inorderTraversal(binarySearchTree.root);
+
+        /* After removing 45
+                  50
+                /     \
+               40     70
+              / \    / \
+             20 42  60 80
+             40
+            / \
+            33
+         */
+        binarySearchTree.deleteNode(binarySearchTree.root, 45);
+        System.out.println("\nAfter deleting 45");
+        binarySearchTree.inorderTraversal(binarySearchTree.root);
+
+          /* After removing 20
+                  50
+                /     \
+               40     70
+                \    / \
+                42  60 80
+         */
+        binarySearchTree.deleteNode(binarySearchTree.root, 20);
+        System.out.println("\nAfter deleting 20");
+        binarySearchTree.inorderTraversal(binarySearchTree.root);
+        System.out.println();
+
+        // 50 - 40 - 42 - 70 - 60 - 80
+        System.out.println("Preorder Traversal");
+        binarySearchTree.preOrderTraversal(binarySearchTree.root);
+        System.out.println();
+
+        // 42 - 40 - 60 - 80 - 70 - 50
+        System.out.println("Postorder Traversal");
+        binarySearchTree.postOrderTraversal(binarySearchTree.root);
+    }
+}
